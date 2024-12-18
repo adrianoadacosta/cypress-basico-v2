c9181839a35f0d50a84bf0913faf98a5e96cfecd
adrianoadacosta <adrianoadacosta@outlook.com>
2024-12-18 11:41:57 -0300

    selecionando opções em campos de seleção suspensa

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -45,11 +45,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.error').should('be.visible')
     })
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
-        cy.get('#firstName').type('Adriano').should('have.value', 'Adriano').clear().should('have.value', '')
-        cy.get('#lastName').type('Costa').should('have.value', 'Costa').clear().should('have.value', '')
+        cy.get('#firstName').type('Adriano')
+        .should('have.value', 'Adriano')
+        .clear()
+        .should('have.value', '')
+        cy.get('#lastName').type('Costa').should('have.value', 'Costa')
+        .clear().should('have.value', '')
         cy.get('#email').type('adriano@teste.com').should('have.value', 'adriano@teste.com')
         .clear().should('have.value', '')
-        cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('have.value', '')      
+        cy.get('#phone').type('123456789').should('have.value', '123456789')
+        .clear().should('have.value', '')      
     })
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
         // vou usar o contains nno lugar do get, outra maneira de usar
@@ -60,6 +65,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
+    })
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product').select('YouTube').should('have.value', 'youtube')
+    })
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+    })
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product').select(1).should('have.value', 'blog')
     })
   })
   
